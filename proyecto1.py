42171fa6d1c19f151c7d6e2978349c84bfcf934f
KarenCaballeros <anakarencaballeros@gmail.com>
2016-05-25 20:45:05 -0600

    Añadi problemas

--- proyecto1.py
+++ proyecto1.py
@@ -1,2 +1,21 @@
 def prob_1(n):
-	return a%2 == 0
+	return n%2 == 0
+
+def prob_2(f):
+	return (f-32)* (5/9)
+
+def prob_3(b,p):
+	cont= 0
+	while cont > p :
+		res= b * b
+		cont= cont + 1 
+	return res 	
+
+def prob_4(pal,lon):
+	list(pal)
+	lol= len(pal)
+	longi= lon - lol
+	res= ("*" * (longi/2), pal , "*" * (longi/2))
+	return res
+
+
